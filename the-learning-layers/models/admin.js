9f2a4d4fdb11607ce1c8966f8de21c5f59c344e6
Atharva Jagtap <atharvauni2021@gmail.com>
2024-03-10 12:49:25 -0700

    Updated admin schema

--- the-learning-layers/models/admin.js
+++ the-learning-layers/models/admin.js
@@ -1,1 +1,12 @@
-//schema goes here
+const mongoose = require('mongoose'); // Import MongoDB methods
+
+// Creating the schema for the Admin model
+const adminSchema = new mongoose.Schema({
+    username: { type: String, required: true }, // Admin's username
+    email: { type: String, required: true, unique: true }, // Admin's email
+    password: { type: String, required: true } // Admin's password
+});
+
+const Admin = mongoose.model('Admin', adminSchema); // Creating the Admin model in the database
+
+module.exports = Admin; // Exporting the model for use in other parts of the application
