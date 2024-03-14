833dfb196812a222cac59eee70782aa0f037e96a
abstractafua <113399971+abstractafua@users.noreply.github.com>
2024-03-13 17:23:49 -0700

    import React

--- the-learning-layers/models/teacher.js
+++ the-learning-layers/models/teacher.js
@@ -4,7 +4,7 @@ const mongoose=require('mongoose'); //use to import MongoDB methods
 const userSchema = new mongoose.Schema({
     firstName: { type: String, required: true }, //we get the username from the frontend and same for other fields
     lastName: { type: String, required: true },
-    studentNum : { type: Number, required: true },
+    studentrNum : { type: Number, required: true },
     position: {type:String, required:true},
     username: { type: String, required: true },
     email: { type: String, required: true },
