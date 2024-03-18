adf663b0c26c5e464fbfd83c8b2680ec6eb44b93
Atharva Jagtap <atharvauni2021@gmail.com>
2024-03-18 16:55:26 -0700

    Changes?

--- the-learning-layers/src/studentPage.js
+++ the-learning-layers/src/studentPage.js
@@ -2,10 +2,12 @@
 import React, { useEffect, useState } from 'react';
 import './App.css';
 import Container from 'react-bootstrap/Container';
+import Navbar from 'react-bootstrap/Navbar';
+import Nav from 'react-bootstrap/Nav';
 import Button from 'react-bootstrap/Button';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
-import StudentMenu from './StudentMenu';
+
 function StudentPage() {
   // Placeholder data for courses, this would likely be fetched from a database in a real application.
   /*
@@ -47,7 +49,22 @@ function StudentPage() {
 
   return (
     <>
-    <StudentMenu></StudentMenu>  
+      <Navbar bg="light" expand="lg">
+        <Container>
+        <Navbar.Brand href="#home">Learning Layers</Navbar.Brand>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="me-auto">
+              <Nav.Link href="#dashboard">Dashboard</Nav.Link>
+              <Nav.Link href="#courses">Courses</Nav.Link>
+              <Nav.Link href="#Enroll">Enroll in a Course</Nav.Link>
+            </Nav>
+            <Nav>
+              <Nav.Link href="#profile">Profile</Nav.Link>
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
       <Container className="my-5">
         <h1 className="text-center mb-4">WELCOME TO LEARNING LAYERS</h1>
         <h2 className="text-center mb-3">A better way to learn, anywhere and anytime.</h2>
@@ -62,15 +79,12 @@ function StudentPage() {
                 <h3 style={{ color: 'white' }} className="text-center my-3">{course.name} {course.courseId}</h3>
                 <div className="text-center">
                   <Button variant="primary">View Course</Button>
-
                 </div>
               </div>
             </Col>
           ))}
         </Row>
-
-        </div>
-     
+        {/* </div> */}
       </Container>
       <footer className=" mt-auto py-3 bg-light text-center">
         <span className="text-muted">© 2023 E-Learning Platform, Inc. All rights reserved.</span>
